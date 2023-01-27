def7c10ccfe0a9510f6ba515efc03a0514d109a6
Igor Zivkovic <igor.zivkovic@plassido.com>
2023-01-27 18:21:36 +0100

    allow cross origin requests

--- klupa-backend/src/main/java/com/plassido/klupa/klupabackend/controller/CategoryController.java
+++ klupa-backend/src/main/java/com/plassido/klupa/klupabackend/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.plassido.klupa.klupabackend.controller;
 import com.plassido.klupa.klupabackend.model.Category;
 import com.plassido.klupa.klupabackend.service.CategoryService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +15,13 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @GetMapping("/{categoryId}")
+    @CrossOrigin
     Category getCategoryById(@PathVariable String categoryId) {
         return categoryService.getCategoryById(categoryId);
     }
 
     @GetMapping({"/child-categories", "/child-categories/{parentId}"})
+    @CrossOrigin
     List<Category> getChildCategories(@PathVariable Optional<String> parentId) {
         return parentId.isPresent() ? categoryService.getChildCategories(parentId.get()) : categoryService.getChildCategories(null);
     }
